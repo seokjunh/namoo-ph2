82f8b96f73ecc5198c9d34f24aafccf8f2956d11
seokjunh <seokjunh97@gmail.com>
2025-06-26 14:23:18 +0900

    update front

--- front-next-ph2/src/components/HomeContent.tsx
+++ front-next-ph2/src/components/HomeContent.tsx
@@ -2,16 +2,18 @@ import HomeItem1 from "./HomeItem1";
 import HomeItem2 from "./HomeItem2";
 import HomeItem3 from "./HomeItem3";
 import HomeItem4 from "./HomeItem4";
+import Partners from "./partners";
 
 const HomeContent = () => {
   return (
     <div>
       <HomeItem1 />
-      <div className="mx-auto max-w-[120rem] space-y-16 py-[6.25rem]">
+      <div className="mx-auto max-w-[120rem] space-y-16 py-[6.25rem] ">
         <HomeItem2 />
         <HomeItem3 />
         <HomeItem4 />
       </div>
+      <Partners />
     </div>
   );
 };
